5401e1bd96f50da5dc7cc33e9c3b2edc059a11f2
busaneko <busaneko.works@gmail.com>
2017-01-09 10:46:51 +0900

    DetailActivityをDataBinding仕様に変更

--- app/src/main/java/com/busanekoworks/laundryicons/DetailActivity.java
+++ app/src/main/java/com/busanekoworks/laundryicons/DetailActivity.java
@@ -1,7 +1,12 @@
 package com.busanekoworks.laundryicons;
 
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.busanekoworks.laundryicons.databinding.ActivityDetailBinding;
 
 import java.util.ArrayList;
 
@@ -10,9 +15,15 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-
         int category = getIntent().getIntExtra(IntentKey.CATEGORY, 0);
         ArrayList<Item> items = Detail.getItemsForCategory(category);
+
+        ActivityDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+        binding.setItems(items);
+    }
+
+    @BindingAdapter("app:items")
+    public static void setItems(ListView view, ArrayList<Item> items){
+        // TODO: DetailAdapterをListViewにSetする
     }
 }
